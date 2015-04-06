8f5dde056109b5b3031c407d8f09f9924eb70ca4
Admin <dmitry.n@pdffiller.com>
2015-04-06 21:11:12 +0300

    Lesson 6

--- src/com/company/ChromeDriver.java
+++ src/com/company/ChromeDriver.java
@@ -6,12 +6,35 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 import javax.swing.plaf.ListUI;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by Dimonaz on 31.03.2015.
  */
 public class ChromeDriver {
+
+    public static String generatePassword (WebDriver driver, String master, String site){
+
+        WebElement masterField = driver.findElement(By.name("master"));
+        masterField.clear();
+        masterField.sendKeys(master);
+        WebElement siteField = driver.findElement(By.name("site"));
+        siteField.clear();
+        siteField.sendKeys(site, Keys.ENTER);
+        WebElement passwordField = driver.findElement(By.name("password"));
+        String password = passwordField.getAttribute("value");
+        return password;
+
+    }
+
+    public static void compare (WebDriver driver, String a, String b, String example){
+
+        String generated = generatePassword(driver, a, b);
+        if (generated.equals(example)){
+            System.out.println("Test PASSED!");
+        } else System.out.println("Test FAILED!");
+    }
 
     public static void main(String[] args) throws InterruptedException {
 
@@ -20,7 +43,8 @@ public class ChromeDriver {
         WebDriver driver = new org.openqa.selenium.chrome.ChromeDriver();
 
         driver.manage().window().maximize();
-        driver.get("http://www.google.com");
+        driver.get("http://angel.net/~nic/passwd.current.html");
+        /*driver.get("http://www.google.com");
 
         String title = driver.getTitle();
         WebElement searchField = driver.findElement(By.id("lst-ib"));
@@ -40,7 +64,10 @@ public class ChromeDriver {
 
         searchField.clear();
         searchField.sendKeys("new search");
-        driver.findElement(By.name("btnG")).click();
+        driver.findElement(By.name("btnG")).click();*/
+
+        compare(driver, "MyPassword123","dmitriy.naydenov@gmail.com", "lUhqIJa8e9/bl@1a");
+        compare(driver, "","", "BaefBs8/Z/cm2@1a");
 
         //driver.quit();
 

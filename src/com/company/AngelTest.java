eeeeb4474b542097ec731b4d7ad25f8264f4cb2e
Admin <dmitry.n@pdffiller.com>
2015-04-20 21:15:57 +0300

    Lesson 8(xpath)

--- src/com/company/AngelTest.java
+++ src/com/company/AngelTest.java
@@ -10,7 +10,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import java.util.List;
 
 /**
  * Created by Admin on 08.04.15.
@@ -22,7 +21,6 @@ public class AngelTest {
 
     public WebDriver driver;
     WebElement buttonGenerate;
-    List<WebElement> td;
 
     @After
 
@@ -34,19 +32,20 @@ public class AngelTest {
 
     @Before
 
-    public void initialize() {
+    public void initialize() throws InterruptedException {
 
         System.setProperty("webdriver.chrome.driver", "C:/chromedriver.exe");
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         //driver.get("http://angel.net/~nic/passwd.current.html");
-        driver.get("http://oxogamestudio.com/passwd.current8.htm");
-        List<WebElement> input = driver.findElements(By.tagName("input"));
-        Homework_6.buttonGenerate = buttonGenerate = input.get(2);
-        Homework_6.masterField = driver.findElement(By.name("master"));
-        Homework_6.siteField = driver.findElement(By.name("site"));
-        Homework_6.passwordField = driver.findElement(By.name("password"));
-        td = driver.findElements(By.tagName("td"));
+        driver.get("http://oxogamestudio.com/passwd.current7.htm");
+        while (driver.findElements(By.xpath("//input[@value='Generate']")).size() <= 0){
+            Thread.sleep(100);
+        }
+        Homework_6.buttonGenerate = driver.findElement(By.xpath("//input[@value='Generate']"));
+        Homework_6.masterField = driver.findElement(By.xpath("//input[@type='password']"));
+        Homework_6.siteField = driver.findElement(By.xpath("(//tr/td/input)[2]"));
+        Homework_6.passwordField = driver.findElement(By.xpath("(//tr/td/input)[4]"));
 
     }
 
@@ -83,21 +82,44 @@ public class AngelTest {
 
     public String getMasterLabel(){
 
-    String s = td.get(0).getText();
+    String s = driver.findElement(By.xpath("//td[text()='Your master password']")).getText();
+
     return s;
 
     }
 
     public String getSiteLabel(){
 
-        String s = td.get(2).getText();
+        String s = driver.findElement(By.xpath("//td[text()='Site name']")).getText();
         return s;
     }
 
     public String getPasswordLabel(){
 
-        String s = td.get(5).getText();
+        String s = driver.findElement(By.xpath("//td[text()='Generated password']")).getText();
         return s;
+    }
+
+    public void titleIsCorrect (){
+
+        setField("Your master password", "12345678");
+        setField("Site name","gmail.com");
+        generate();
+        String pwd = getField("Generated password");
+        Assert.assertEquals("W3Hdka0clbEI+@1a", pwd);
+
+    }
+
+    public void setField (String labelName, String fieldValue){
+
+        int fieldNumber = driver.findElements(By.xpath("//td[text()='" + labelName + "'")).size();
+        WebElement field = driver.findElement(By.xpath(""));
+
+        for (int i = 0; i < fieldNumber; i++){
+
+            field.sendKeys(fieldValue);
+        }
+
     }
 
     @Test
@@ -117,6 +139,10 @@ public class AngelTest {
     public void Test2() throws Exception{
 
         Assert.assertTrue("Password did not match.", compare("", "gmail.com", "zmcHOAyf2oZm+@1a"));
+        Assert.assertTrue(verifyFieldsEnabled());
+        Assert.assertEquals(getMasterLabel(), "Your master password");
+        Assert.assertEquals(getSiteLabel(), "Site name");
+        Assert.assertEquals(getPasswordLabel(), "Generated password");
 
     }
 
@@ -126,6 +152,10 @@ public class AngelTest {
     public void Test3() throws Exception{
 
         Assert.assertTrue("Password did not match.", compare("12345678", "", "9Ixm2r5Xnm41Q@1a"));
+        Assert.assertTrue(verifyFieldsEnabled());
+        Assert.assertEquals(getMasterLabel(), "Your master password");
+        Assert.assertEquals(getSiteLabel(), "Site name");
+        Assert.assertEquals(getPasswordLabel(), "Generated password");
 
     }
 
@@ -136,6 +166,10 @@ public class AngelTest {
 
         Homework_6.generate();
         Assert.assertTrue("Password did not match.", compare("", "", "BaefBs8/Z/cm2@1a"));
+        Assert.assertTrue(verifyFieldsEnabled());
+        Assert.assertEquals(getMasterLabel(), "Your master password");
+        Assert.assertEquals(getSiteLabel(), "Site name");
+        Assert.assertEquals(getPasswordLabel(), "Generated password");
 
     }
 
@@ -155,6 +189,11 @@ public class AngelTest {
         Homework_6.generate();
         String password = Homework_6.getPassword();
         Assert.assertEquals("Password did not match!", password, "aR8ztwNBbSqe5@1a");
+        Assert.assertTrue(verifyNotCleared());
+        Assert.assertTrue(verifyFieldsEnabled());
+        Assert.assertEquals(getMasterLabel(), "Your master password");
+        Assert.assertEquals(getSiteLabel(), "Site name");
+        Assert.assertEquals(getPasswordLabel(), "Generated password");
 
     }
 
@@ -168,6 +207,11 @@ public class AngelTest {
         Homework_6.generate();
         String password = Homework_6.getPassword();
         Assert.assertEquals("Password did not match!", password, "ctolW6AdI0te1@1a");
+        Assert.assertTrue(verifyNotCleared());
+        Assert.assertTrue(verifyFieldsEnabled());
+        Assert.assertEquals(getMasterLabel(), "Your master password");
+        Assert.assertEquals(getSiteLabel(), "Site name");
+        Assert.assertEquals(getPasswordLabel(), "Generated password");
 
     }
 
@@ -178,6 +222,10 @@ public class AngelTest {
 
         String buttonLabel = buttonGenerate.getAttribute ("value");
         Assert.assertEquals("Label did not match!", buttonLabel, "Generate");
+        Assert.assertTrue(verifyFieldsEnabled());
+        Assert.assertEquals(getMasterLabel(), "Your master password");
+        Assert.assertEquals(getSiteLabel(), "Site name");
+        Assert.assertEquals(getPasswordLabel(), "Generated password");
 
     }
 
--- src/com/company/Homework_6.java
+++ src/com/company/Homework_6.java
@@ -4,9 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+
 
 import java.util.List;
 
@@ -53,7 +51,6 @@ public class Homework_6 {
 
     public static void generate() throws InterruptedException {
 
-    String password = "";
     buttonGenerate.click();
 
     while (passwordField.getAttribute("value").equals(""))
